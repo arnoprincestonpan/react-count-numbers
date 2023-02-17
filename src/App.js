7ef69827fad1a469cc23e941ad63614a8c88026d
panarno <78936893+panarno@users.noreply.github.com>
2023-02-17 02:52:56 -0800

    complete working counts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
 
 function App() {
+  const [startNum, setStartNum] = useState(0)
+  const [endNum, setEndNum] = useState(0)
+  const [display, setDisplay] = useState([])
+  let msg = [];
+
+  function count(){
+    console.log("counting...")
+    console.log("Starting from: " + startNum)
+    console.log("Ending from: " + endNum)
+    let range = Math.abs(startNum - endNum)
+    if(startNum > endNum){
+      console.log("counting downwards...")
+      msg.push(<li>Counting downwards...</li>)
+      for(let i = 0; i <= range; i++){
+        msg.push(<li key={i}>{startNum - i}</li>)
+      }
+    }else if(endNum > startNum){
+      console.log("counting upwards...")
+      msg.push(<li>Counting upwards...</li>)
+      for(let i = 0; i <= range; i++){
+        msg.push(<li key={i}>{startNum + i}</li>)
+      }
+    }else if(endNum === startNum){
+      msg.push(<li>No Counting.</li>)
+      console.log("No counting...")
+    }
+    setDisplay(msg)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1 id="title">Counting Numbers, Up & Down</h1>
+      <section>
+        <label htmlFor="start-num">Counting From: </label>
+        <input onChange={(e) => setStartNum(e.target.valueAsNumber)} name="start-num" type="number" placeholder="Enter a Number to Start"></input>
+        <br></br>
+        <label htmlFor="end-num">Counting To: </label>
+        <input onChange={(e) => setEndNum(e.target.valueAsNumber)} name="end-num" type="number" placeholder="Enter a Number to End"></input>
+        <br></br>
+        <input onClick={() => count()} type="button" value="Submit"></input>
+      </section>
+      <section>
+        <ul>
+          {display}
+        </ul>
+      </section>
+    </main>
   );
 }
 
